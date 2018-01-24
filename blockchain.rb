996c07a0a962aa7a37b8ef0dfb18f16726585be1
Nick Pachulski <nick@pachulski.me>
2018-01-24 11:31:20 -0500

    Simpler blockchain

--- blockchain.rb
+++ blockchain.rb
@@ -1,12 +1,9 @@
-require 'sinatra'
 require 'digest'
 require 'random-word'
 
 class Block
-  attr_reader :id, :position, :stuff
-
-  def initialize(position:, stuff:, parent_id:)
-    @position  = position
+  def initialize(index:, stuff:, parent_id:)
+    @index     = index
     @time      = Time.new
     @stuff     = stuff
     @parent_id = parent_id
@@ -14,51 +11,38 @@ class Block
   end
 
   def to_s
-    "Block #{position}: #{stuff} (#{id})"
+    "Block #{@index} (#{@id}): #{@stuff}"
+  end
+
+  def next(next_stuff)
+    Block.new(
+      index: @index + 1,
+      stuff: next_stuff,
+      parent_id: @id,
+    )
+  end
+
+  def self.genesis
+    Block.new(
+      index: 0,
+      stuff: "genesis",
+      parent_id: "¯\_(ツ)_/¯",
+    )
   end
 
   private
 
   def identity
-    Digest::SHA1.hexdigest("#{@position}#{@time}#{@stuff}#{@parent_id}")
+    Digest::SHA1.hexdigest("#{@index}#{@time}#{@stuff}#{@parent_id}")
   end
 end
 
-def next_block(parent:, stuff:)
-  Block.new(
-    position: parent.position + 1,
-    stuff: stuff,
-    parent_id: parent.id,
-  )
+blockchain = [Block.genesis]
+
+4.times do |number|
+  blockchain << blockchain.last.next("#{RandomWord.adjs.next}")
 end
 
-def new_transaction(from, to, amount)
-  {
-    from: from,# RandomWord.adjs.next,
-    to: to,# RandomWord.nouns.next,
-    amount: amount, # rand(1..10),
-  }
-end
-
-genesis = Block.new(
-  position: 0,
-  stuff: "genesis",
-  parent_id: "¯\_(ツ)_/¯"
-)
-
-blockchain = [genesis]
-
-observed_transactions = []
-
-# create transaction
-post "/transaction" do
-  transaction_json = JSON.parse(request.body.read)
-  observed_transactions << new_transaction(
-    transaction_json["from"],
-    transaction_json["to"],
-    transaction_json["amount"],
-  )
-  observed_transactions.to_s
-end
-
-
+puts
+puts blockchain
+puts
